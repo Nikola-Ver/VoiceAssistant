ce14110754b0be311dd9122c19b004ff1b5258ca
Nikola-Ver <kolia_ver@bk.ru>
2021-08-03 17:50:22 +0300

    add: active noise cancellation

--- src/index.py
+++ src/index.py
@@ -8,7 +8,9 @@ with sr.Microphone() as source:
     is_listening_command = False
     while True:
         try:
-            audio = r.listen(source)
+            r.adjust_for_ambient_noise(source)
+            r.energy_threshold = 300 
+            audio = r.listen(source, phrase_time_limit=7)
             phrase = r.recognize_google(audio)
             is_listening_command = commands.recognize_command(phrase, is_listening_command)
         except: 
